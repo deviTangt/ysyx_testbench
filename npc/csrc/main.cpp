f09527510d22fd1803f1e37418af842e0915e321
deviT <dave43225977@outlook.com>
2026-08-18 22:12:43 +0800

    config npc NVBoard Fin

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,17 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <nvboard.h>
 #include "Vtop.h"
 #include "verilated.h"
 #include "verilated_fst_c.h"
 
-int main(int argc, char** argv) {
+Vtop* top;
+void nvboard_bind_all_pins(Vtop* top);
 
+static void single_cycle() {
+  top->clk = 0; top->eval();
+  top->clk = 1; top->eval();
+}
+
+static void reset(int n) {
+  top->rst = 1;
+  while (n -- > 0) single_cycle();
+  top->rst = 0;
+}
+
+int main(int argc, char** argv) {
   VerilatedContext* contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
-  Vtop* top = new Vtop{contextp};
-  
+
+  top = new Vtop{contextp};
+
   printf("Hello, ysyx!\n");
+  nvboard_bind_all_pins(top);
+  nvboard_init(0);
+
+  reset(10);
   
   // 开启波形记录
   Verilated::traceEverOn(true);
@@ -21,15 +40,10 @@ int main(int argc, char** argv) {
   tfp->open("wave.fst");     // 输出 FST 文件
   
   int cnt_loop = 0;
-  while (!contextp->gotFinish() && cnt_loop < 20) {
-    int a = rand() & 1;
-    int b = rand() & 1;
-    top->a = a;
-    top->b = b;
-    top->eval();
-    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-    assert(top->f == (a ^ b));
-    
+  while (1) {
+    nvboard_update();
+    single_cycle();
+
     tfp->dump(contextp->time()); // 把当前时刻写入波形
     contextp->timeInc(1);        // 仿真时间前进 1
     
