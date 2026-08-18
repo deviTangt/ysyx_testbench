11e0f1f49241da3f0fd3961221c6a773ae6a62a1
tracer-ysyx <tracer@ysyx.org>
2026-08-18 22:12:05 +0800

    > sim RTL ysyx_22040330 唐德伟 Linux HUST 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:12:05 up  6:56,  1 user,  load average: 0.50, 0.49, 0.37

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -6,28 +6,29 @@
 #include "verilated.h"
 #include "verilated_fst_c.h"
 
-static TOP_NAME top;
-
-void nvboard_bind_all_pins(TOP_NAME* top);
+Vtop* top;
+void nvboard_bind_all_pins(Vtop* top);
 
 static void single_cycle() {
-  top.clk = 0; top.eval();
-  top.clk = 1; top.eval();
+  top->clk = 0; top->eval();
+  top->clk = 1; top->eval();
 }
 
 static void reset(int n) {
-  top.rst = 1;
+  top->rst = 1;
   while (n -- > 0) single_cycle();
-  top.rst = 0;
+  top->rst = 0;
 }
 
 int main(int argc, char** argv) {
   VerilatedContext* contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
 
+  top = new Vtop{contextp};
+
   printf("Hello, ysyx!\n");
-  nvboard_bind_all_pins(&top);
-  nvboard_init();
+  nvboard_bind_all_pins(top);
+  nvboard_init(0);
 
   reset(10);
   
