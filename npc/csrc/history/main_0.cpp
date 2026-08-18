a1a60ea2c4985d5a2d0d0134b8da4088669e2ebb
tracer-ysyx <tracer@ysyx.org>
2026-08-18 21:09:24 +0800

    > sim RTL ysyx_22040330 唐德伟 Linux HUST 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:09:24 up  5:53,  1 user,  load average: 1.06, 0.50, 0.30

--- npc/csrc/history/main_0.cpp
+++ npc/csrc/history/main_0.cpp
@@ -6,30 +6,28 @@
 #include "verilated.h"
 #include "verilated_fst_c.h"
 
-static TOP_NAME top;
+static TOP_NAME dut;
 
 void nvboard_bind_all_pins(TOP_NAME* top);
 
 static void single_cycle() {
-  top.clk = 0; top.eval();
-  top.clk = 1; top.eval();
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
 }
 
 static void reset(int n) {
-  top.rst = 1;
+  dut.rst = 1;
   while (n -- > 0) single_cycle();
-  top.rst = 0;
+  dut.rst = 0;
 }
 
 int main(int argc, char** argv) {
+
   VerilatedContext* contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
-
+  Vtop* top = new Vtop{contextp};
+  
   printf("Hello, ysyx!\n");
-  nvboard_bind_all_pins(&top);
-  nvboard_init();
-
-  reset(10);
   
   // 开启波形记录
   Verilated::traceEverOn(true);
@@ -39,10 +37,15 @@ int main(int argc, char** argv) {
   tfp->open("wave.fst");     // 输出 FST 文件
   
   int cnt_loop = 0;
-  while (1) {
-    nvboard_update();
-    single_cycle();
-
+  while (!contextp->gotFinish() && cnt_loop < 20) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    assert(top->f == (a ^ b));
+    
     tfp->dump(contextp->time()); // 把当前时刻写入波形
     contextp->timeInc(1);        // 仿真时间前进 1
     
