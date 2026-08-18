28292ed205fef39b497e6b5a54b905e9572aa96f
tracer-ysyx <tracer@ysyx.org>
2026-08-18 18:03:15 +0800

    > sim RTL ysyx_22040330 唐德伟 Linux HUST 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:03:15 up  2:47,  1 user,  load average: 0.70, 0.26, 0.19

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
 int main() {
   printf("Hello, ysyx!\n");
+  
+  int cnt_loop = 0;
+  while (cnt_loop < 20) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    assert(top->f == (a ^ b));
+    
+    cnt_loop ++;
+  }
+
   return 0;
 }
