ab9e72c1b8954a4db8e1106f02de4b35b87622a8
tracer-ysyx <tracer@ysyx.org>
2026-08-18 18:59:02 +0800

    > sim RTL ysyx_22040330 唐德伟 Linux HUST 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:59:02 up  3:43,  1 user,  load average: 0.71, 0.34, 0.18

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include "Vtop.h"
 #include "verilated.h"
+#include "verilated_fst_c.h"
 
 int main(int argc, char** argv) {
 
@@ -11,6 +12,13 @@ int main(int argc, char** argv) {
   Vtop* top = new Vtop{contextp};
   
   printf("Hello, ysyx!\n");
+  
+  // 开启波形记录
+  Verilated::traceEverOn(true);
+  
+  VerilatedFstC* tfp = new VerilatedFstC;
+  top->trace(tfp, 99);       // 99 表示记录 99 层层次
+  tfp->open("wave.fst");     // 输出 FST 文件
   
   int cnt_loop = 0;
   while (!contextp->gotFinish() && cnt_loop < 20) {
@@ -22,9 +30,14 @@ int main(int argc, char** argv) {
     printf("a = %d, b = %d, f = %d\n", a, b, top->f);
     assert(top->f == (a ^ b));
     
+    tfp->dump(contextp->time()); // 把当前时刻写入波形
+    contextp->timeInc(1);        // 仿真时间前进 1
+    
     cnt_loop ++;
   }
 
+  tfp->close();
+  delete tfp;
   delete top;
   delete contextp;
   return 0;
