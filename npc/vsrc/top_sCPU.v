0b2de30fe293983e4de24ce457f73f7a09ec50d8
tracer-ysyx <tracer@ysyx.org>
2026-08-20 01:02:30 +0800

    > sim RTL ysyx_22040330 唐德伟 Linux HUST 7.0.0-29-generic #29~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 12 17:25:56 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:02:30 up  4:54,  1 user,  load average: 0.62, 0.52, 0.40

--- npc/vsrc/top_sCPU.v
+++ npc/vsrc/top_sCPU.v
@@ -39,7 +39,7 @@ reg  [4:0] btn_p;
 Reg #(5, 5'd0) r_btn (clk, rst, btn, btn_p, 1'b1);
 wire [4:0] btn_pos = ~btn_p & btn;
 Reg #(1, 1'd0) r_sysclk (clk, rst, ~sys_clk, sys_clk, btn_pos[4]);
-assign sys_rst = btn[2] | rst;
+assign sys_rst = ~btn[2] | rst;
 
 //? PC
 reg [7:0] rom_PC [255:0];
