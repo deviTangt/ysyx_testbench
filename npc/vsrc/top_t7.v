c77b89cc82269da27618bc4545c50cbe75930152
tracer-ysyx <tracer@ysyx.org>
2026-08-19 21:12:30 +0800

    > sim RTL ysyx_22040330 唐德伟 Linux HUST 7.0.0-29-generic #29~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 12 17:25:56 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:12:30 up  1:04,  1 user,  load average: 0.39, 0.47, 0.34

--- npc/vsrc/top_t7.v
+++ npc/vsrc/top_t7.v
@@ -46,19 +46,23 @@ ps2_keyboard ps2(clk, ~(rst | btn_pos[4] | ps2_of), ps2_clk, ps2_data, ps2_data_
 Reg #(8, 8'h00) r0 (clk, rst, ps2_data_out, ps2_code, ps2_data_valid);
 assign ps2_ready_get_n = 1'b0;
 
+
 reg [7:0] ps2_code_pre;
 reg [7:0] ps2_cpde_pre_valid;
-wire ps2_tick_off;
+wire ps2_tick_off = (ps2_code_pre == 8'hf0);
 wire ps2_diff = (ps2_code != 8'hf0) && (ps2_code_pre != 8'hf0) && (ps2_code != ps2_code_pre);
 
 Reg #(8, 8'h00) r1 (clk, rst, ps2_code, ps2_code_pre, ps2_data_valid);
 Reg #(8, 8'h00) r2 (clk, rst, ps2_code_pre, ps2_cpde_pre_valid, ps2_diff);
 
+reg [7:0] cnt_tick;
+Reg #(8, 8'h00) r3 (clk, rst, cnt_tick + 8'd1, cnt_tick, ps2_tick_off);
+
 //output debuginfo to bcd
-bcd7seg_AF ins_seg7(.b(ps2_cpde_pre_valid[7:4]), .h(seg7));
-bcd7seg_AF ins_seg6(.b(ps2_cpde_pre_valid[3:0]), .h(seg6));
-bcd7seg_AF ins_seg5(.b(ps2_code_pre[7:4]), .h(seg5));
-bcd7seg_AF ins_seg4(.b(ps2_code_pre[3:0]), .h(seg4));
+bcd7seg_AF ins_seg7(.b(cnt_tick[7:4]), .h(seg7));
+bcd7seg_AF ins_seg6(.b(cnt_tick[3:0]), .h(seg6));
+bcd7seg_AF ins_seg5(.b(ps2_cpde_pre_valid[7:4]), .h(seg5));
+bcd7seg_AF ins_seg4(.b(ps2_cpde_pre_valid[3:0]), .h(seg4));
 bcd7seg_AF ins_seg3(.b(4'd0), .h(seg3));
 bcd7seg_AF ins_seg2(.b(4'd0), .h(seg2));
 bcd7seg_AF ins_seg1(.b(ps2_code[7:4]), .h(seg1));
