348d0f6e969bf13b42570fb9c2a83c38bfd597ae
tracer-ysyx <tracer@ysyx.org>
2026-08-19 15:03:50 +0800

    > sim RTL ysyx_22040330 唐德伟 Linux HUST 7.0.0-29-generic #29~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 12 17:25:56 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:03:50 up 45 min,  1 user,  load average: 0.17, 0.26, 0.18

--- npc/vsrc/top_t3.v
+++ npc/vsrc/top_t3.v
@@ -90,16 +90,20 @@ MuxKeyWithDefault #(8, 3, 4) i0 (result, op, 4'd0, {
     3'd7, res_op_equ
   });
 
+wire [3:0] num1_w1 = {3'd0, num1 > 4'd9};
+wire [3:0] num1_w0 = num1 > 4'd9 ? num1 - 4'd10 : num1;
+wire [3:0] num2_w1 = {3'd0, num2 > 4'd9};
+wire [3:0] num2_w0 = num2 > 4'd9 ? num2 - 4'd10 : num2;
+wire [3:0] res_w1 = {3'd0, result > 4'd9};
+wire [3:0] res_w0 = result > 4'd9 ? result - 4'd10 : result;
 //output debuginfo to bcd
-bcd7seg ins_seg7(.b(num1), .h(seg7));
-// bcd7seg ins_seg6(.b(4'd0), .h(seg6));
-assign seg6 = 8'hff;
-bcd7seg ins_seg5(.b(num2), .h(seg5));
-// bcd7seg ins_seg4(.b(4'd0), .h(seg4));
-assign seg4 = 8'hff;
+bcd7seg ins_seg7(.b(num1_w1), .h(seg7));
+bcd7seg ins_seg6(.b(num1_w0), .h(seg6));
+bcd7seg ins_seg5(.b(num2_w1), .h(seg5));
+bcd7seg ins_seg4(.b(num2_w0), .h(seg4));
 bcd7seg ins_seg3(.b({3'd0, f_zero}), .h(seg3));
-bcd7seg ins_seg2(.b({3'd0, f_of}), .h(seg2));
-bcd7seg ins_seg1(.b({3'd0, f_carry}), .h(seg1));
-bcd7seg ins_seg0(.b(result), .h(seg0));
+bcd7seg ins_seg2(.b({2'd0, f_of, f_carry}), .h(seg2));
+bcd7seg ins_seg1(.b(res_w1), .h(seg1));
+bcd7seg ins_seg0(.b(res_w0), .h(seg0));
 
 endmodule
