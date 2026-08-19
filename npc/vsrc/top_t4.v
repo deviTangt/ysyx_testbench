a022daf2b7f3c45cb3b86bb4e8dc1a33821e33a1
tracer-ysyx <tracer@ysyx.org>
2026-08-19 20:07:10 +0800

    > sim RTL ysyx_22040330 唐德伟 Linux HUST 7.0.0-29-generic #29~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 12 17:25:56 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:07:10 up  5:24,  1 user,  load average: 1.14, 1.09, 0.59

---
+++ npc/vsrc/top_t4.v
@@ -0,0 +1,56 @@
+module top(
+    input  wire         clk,
+    input  wire         rst,
+
+    input  wire [4:0]   btn,
+    input  wire [15:0]  sw,
+
+    input  wire         ps2_clk,
+    input  wire         ps2_data,
+
+    input  wire         uart_rx,
+    output wire         uart_tx,
+
+    output wire [15:0]  ledr,
+
+    output wire         VGA_CLK,
+    output wire         VGA_HSYNC,
+    output wire         VGA_VSYNC,
+    output wire         VGA_BLANK_N,
+    output wire [7:0]   VGA_R,
+    output wire [7:0]   VGA_G,
+    output wire [7:0]   VGA_B,
+
+    output wire [7:0]   seg0,
+    output wire [7:0]   seg1,
+    output wire [7:0]   seg2,
+    output wire [7:0]   seg3,
+    output wire [7:0]   seg4,
+    output wire [7:0]   seg5,
+    output wire [7:0]   seg6,
+    output wire [7:0]   seg7
+
+);
+
+reg  [4:0] btn_p;
+Reg #(5, 0) r_b (clk, rst, btn, btn_p, 1'b1);
+wire [4:0] btn_pos = ~btn_p & btn;
+
+reg [7:0] lfsr;
+wire [7:0] lfsr_n; 
+assign lfsr_n[6:0] = lfsr[7:1];
+assign lfsr_n[7] = lfsr_n[4] ^ lfsr_n[3] ^ lfsr_n[2] ^ lfsr_n[0];
+Reg #(8, 8'd1) r0 (btn_pos[0], btn_pos[4], lfsr_n, lfsr, 1'b1);
+
+
+//output debuginfo to bcd
+bcd7seg ins_seg7(.b(4'd14), .h(seg7));
+bcd7seg ins_seg6(.b(4'd14), .h(seg6));
+bcd7seg ins_seg5(.b(4'd14), .h(seg5));
+bcd7seg ins_seg4(.b(4'd14), .h(seg4));
+bcd7seg ins_seg3(.b(4'd14), .h(seg3));
+bcd7seg ins_seg2(.b(4'd14), .h(seg2));
+bcd7seg ins_seg1(.b(lfsr[7:4]), .h(seg1));
+bcd7seg ins_seg0(.b(lfsr[3:0]), .h(seg0));
+
+endmodule
