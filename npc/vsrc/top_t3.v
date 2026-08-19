f85c2c6b4db73daa7d239209db11d19f7190c1f8
tracer-ysyx <tracer@ysyx.org>
2026-08-19 14:55:18 +0800

    > sim RTL ysyx_22040330 唐德伟 Linux HUST 7.0.0-29-generic #29~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 12 17:25:56 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:55:18 up 37 min,  1 user,  load average: 0.29, 0.15, 0.11

--- npc/vsrc/top_t3.v
+++ npc/vsrc/top_t3.v
@@ -43,6 +43,7 @@ reg  [2:0] op;
 wire [2:0] op_n;
 assign op_n = btn_pos[0] ? (op + 3'b1) : btn_pos[4] ? (op - 3'b1) : (op); 
 Reg #(3, 0) r_m (clk, rst, op_n, op, btn_pos[0] | btn_pos[2] | btn_pos[4]);
+assign ledr[7:0] = (1 << op);
 
 
 wire f_zero;
