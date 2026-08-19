99c52150169508dcf8f501493f87552ff9eeda62
tracer-ysyx <tracer@ysyx.org>
2026-08-19 21:55:16 +0800

    > sim RTL ysyx_22040330 唐德伟 Linux HUST 7.0.0-29-generic #29~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 12 17:25:56 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:55:16 up  1:47,  1 user,  load average: 0.13, 0.19, 0.18

--- npc/vsrc/top_t8.v
+++ npc/vsrc/top_t8.v
@@ -73,13 +73,13 @@ end
 
 
 //output debuginfo to bcd
-// bcd7seg_AF ins_seg7(.b(cnt_tick[7:4]), .h(seg7), .en(1'b1));
-// bcd7seg_AF ins_seg6(.b(cnt_tick[3:0]), .h(seg6), .en(1'b1));
-// bcd7seg_AF ins_seg5(.b(ps2_cpde_pre_valid[7:4]), .h(seg5), .en(1'b0));
-// bcd7seg_AF ins_seg4(.b(ps2_cpde_pre_valid[3:0]), .h(seg4), .en(1'b0));
-// bcd7seg_AF ins_seg3(.b(tx_ascii[7:4]), .h(seg3), .en(ps2_code_pre != 8'hf0));
-// bcd7seg_AF ins_seg2(.b(tx_ascii[3:0]), .h(seg2), .en(ps2_code_pre != 8'hf0));
-// bcd7seg_AF ins_seg1(.b(ps2_code[7:4]), .h(seg1), .en(ps2_code_pre != 8'hf0));
-// bcd7seg_AF ins_seg0(.b(ps2_code[3:0]), .h(seg0), .en(ps2_code_pre != 8'hf0));
+bcd7seg_AF ins_seg7(.b(4'd0), .h(seg7), .en(1'b1));
+bcd7seg_AF ins_seg6(.b(4'd0), .h(seg6), .en(1'b1));
+bcd7seg_AF ins_seg5(.b({2'd0, vc[9:8]}), .h(seg5), .en(hc[9:8] != 0));
+bcd7seg_AF ins_seg4(.b(vc[7:4]), .h(seg4), .en(vc[7:4] != 0));
+bcd7seg_AF ins_seg3(.b(vc[3:0]), .h(seg3), .en(1'b1));
+bcd7seg_AF ins_seg2(.b({2'd0, hc[9:8]}), .h(seg2), .en(hc[9:8] != 0));
+bcd7seg_AF ins_seg1(.b(hc[7:4]), .h(seg1), .en(| hc[7:4]));
+bcd7seg_AF ins_seg0(.b(hc[3:0]), .h(seg0), .en(| hc[3:0]));
 
 endmodule
