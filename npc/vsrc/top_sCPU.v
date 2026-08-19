eb5373eb6f7ede4cd32f1a6f29c3c31c251f5426
tracer-ysyx <tracer@ysyx.org>
2026-08-20 00:45:38 +0800

    > sim RTL ysyx_22040330 唐德伟 Linux HUST 7.0.0-29-generic #29~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 12 17:25:56 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:45:38 up  4:37,  1 user,  load average: 0.35, 0.35, 0.31

--- npc/vsrc/top_sCPU.v
+++ npc/vsrc/top_sCPU.v
@@ -182,7 +182,9 @@ always@(posedge sys_clk) begin
             $display("PC[%02d] Instruct:%08b Type:%s", PC, instrct, "io");
         end
         2'b10: begin //! li
-            $display("%02d: %08b  %02x  li r%1d %1d << %1d(%1d)"
+            $display("%02d: %08b  %02x  li rd imm << s( )"
+                , PC, instrct, instrct, rd, imm, s, imm << s);
+            $display("%02d: %08b  %02x  li r%1d %3d << %1d(%1d)"
                 , PC, instrct, instrct, rd, imm, s, imm << s);
         end
         2'b11: begin //! bner0
